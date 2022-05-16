f45bb12a65798267242069c7177f323355163a17
Yutthanon <630132418003@bru.ac.th>
2022-05-16 10:10:26 +0700

    LAB5

--- LAB5/www/query/dt_member.php
+++ LAB5/www/query/dt_member.php
@@ -1,26 +1,52 @@
 <?php
 
-require("../function/condb.php");
+require("../funtion/condb.php");
 
-$sql ="SELECT * FROM tb_member ORDER BY Username ASC";
-$result = $mysql->query($sql);
+$sql = "SELECT * from tb_member ORDER BY username ASC";
+$result = $mysqli->query($sql);
 ?>
+
 <table>
     <tr>
         <td>ID</td>
         <td>username</td>
         <td>password</td>
+        <td></td>
     </tr>
     <?php
     while ($row = $result->fetch_assoc()) {
     ?>
         <tr>
-            <td><?= $row["ID"] ?></td>
-            <td><?= $row["Username"] ?></td>
+            <td><?= $row["id"] ?></td>
+            <td><?= $row["username"] ?></td>
             <td><?= $row["password"] ?></td>
+            <td><button class="btn_del_member" data-id="<?= $row["id"] ?>">
+                    DEL
+                </button>
+                <button class="btn_edit_member" data-id="<?= $row["id"] ?>">
+                    EDIT
+                </button>
+            </td>
         </tr>
     <?php
     }
     ?>
 </table>
 
+<script>
+    $(".btn_del_member").click(function() {
+        let id = $(this).data("id");
+        //alert(id);
+        $.ajax({
+            url:"./query/del_member.php",
+            method: "GET",
+            data: {
+                id: id
+            },
+            success: function(msg) {
+                //alert(msg);
+                $("#div_dt").load("./query/dt_member.php");
+            }
+        });
+    });
+</script>
